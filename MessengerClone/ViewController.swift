7b179d18aed23a20849914d30f7200a7f05da9d4
Marko Antoljak <marko.antoljak6@gmail.com>
2022-11-28 11:49:56 +0100

    Initial Commit

---
+++ MessengerClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerClone
+//
+//  Created by Marko Antoljak on 11/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
